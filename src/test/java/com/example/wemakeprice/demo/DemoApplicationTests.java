4483abdf0142bc7c98c54bc386837428847b8d59
김원태 <kwt@wemakeprice.com>
2020-06-23 17:25:37 +0900

    initial commit

---
+++ src/test/java/com/example/wemakeprice/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wemakeprice.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
